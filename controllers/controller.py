7253af32602db45c13a2c3cf3fa3ed6725c952c1
codeclan student <codeclanstudent@codeclans-mbp.home>
2022-01-26 15:15:31 +0000

    Added invidual order page, get directory and backend functionality

--- controllers/controller.py
+++ controllers/controller.py
@@ -6,3 +6,7 @@ from models.customer_orders import customer_orders
 def index():
     return render_template('index.html', customer_orders=customer_orders)
 
+@app.route('/orders/<customer_i>')
+def get_single_order(customer_i):
+    customer_i = int(customer_i)
+    return render_template('order.html', customer_orders=customer_orders[customer_i])
